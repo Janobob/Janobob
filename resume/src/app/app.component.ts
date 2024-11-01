124c9ca6f6c35799c3d6e9c027af01145c8ee544
Jan Werren <jan.werren@outlook.com>
2024-11-01 13:24:53 +0100

    feat: add navigation

--- resume/src/app/app.component.ts
+++ resume/src/app/app.component.ts
@@ -1,13 +1,32 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { ActivatedRoute, Router, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'resume';
+  activeFragment: string = 'about-me';
+
+  constructor(private route: ActivatedRoute, private router: Router) {}
+
+  ngOnInit(): void {
+    this.route.fragment.subscribe((fragment) => {
+      this.activeFragment = fragment || 'about-me';
+    });
+  }
+
+  isActive(fragment: string): boolean {
+    return this.activeFragment === fragment;
+  }
+
+  onNavigationClick(fragment: string): void {
+    this.router.navigate([], {
+      fragment: fragment,
+    });
+  }
 }
